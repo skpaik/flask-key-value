6a0625b1600d1d7f38b36e6bec15508c7f3d34c7
sudipta <sk.paik@icloud.com>
2023-01-30 01:52:57 +0600

    Series Key Value feature updated

--- single_ton.py
+++ single_ton.py
@@ -1,6 +1,7 @@
 class SingletonBase(object):
     _instances = {}
     local_map = {}
+    local_map_series = {}
 
     def __new__(class_, *args, **kwargs):
         if class_ not in class_._instances:
@@ -21,3 +22,45 @@ class Singleton(SingletonBase):
 
     def delete(self, key):
         return self.local_map.pop(key, None)
+
+    def put_series1(self, key1, value):
+        self.local_map_series[key1] = value
+
+    def put_series2(self, key1, key2, value):
+        if key1 in self.local_map_series:
+            key1_value = self.local_map_series.get(key1)
+            if not isinstance(key1_value, dict):
+                self.local_map_series[key1] = {}
+        else:
+            self.local_map_series[key1] = {}
+
+        self.local_map_series[key1][key2] = value
+
+    def put_series3(self, key1, key2, key3, value):
+        self.put_series2(key1, key2, {})
+
+        self.local_map_series[key1][key2][key3] = value
+
+    def get_series1(self, key1):
+        return self.local_map_series.get(key1, '')
+
+    def get_series2(self, key1, key2):
+        value = self.local_map_series.get(key1, {})
+        if not isinstance(value, dict):
+            return ''
+
+        return value.get(key2, '')
+
+    def get_series3(self, key1, key2, key3):
+        value = self.local_map_series.get(key1, {})
+        if not isinstance(value, dict):
+            return ''
+
+        value = value.get(key2, '')
+        if not isinstance(value, dict):
+            return ''
+
+        return value.get(key3, '')
+
+    def delete_series1(self, key1):
+        return self.local_map_series.pop(key1, None)
--- tests/kv.py
+++ tests/kv.py
@@ -1,4 +1,4 @@
-from fkv_helper import set_kv, get_kv, delete_kv
+from fkv_helper import set_kv, get_kv, delete_kv, set_series_kv, get_series_kv, delete_series_kv
 
 r = set_kv('a', 291)
 print(r)
@@ -21,4 +21,56 @@ r = get_kv('d')
 print(r)
 
 r = delete_kv('d')
+print(r)
+
+send_time = {
+    'fs': {
+        'h2': 37.99,
+        'h3': 41.889
+    },
+    'ed': {
+        'h2': 56.444,
+        'h3': 77.000
+    }
+}
+
+dict_data = 78
+
+r = set_series_kv(dict_data, 'send_time')
+print(r)
+
+r = set_series_kv(dict_data, 'send_time', 'fs')
+print(r)
+
+r = set_series_kv('37.99', 'send_time', 'fs', 'h2')
+print(r)
+
+r = set_series_kv('41.889', 'send_time', 'fs', 'h3')
+print(r)
+
+r = get_series_kv('send_time')
+print(r)
+r = get_series_kv('send_time', 'fs')
+print(r)
+r = get_series_kv('send_time', 'fs', 'h2')
+print(r)
+r = get_series_kv('send_time', 'fs', 'h3')
+print(r)
+
+r = set_series_kv(dict_data, 'send_time', 'ed')
+print(r)
+
+r = set_series_kv('56.444', 'send_time', 'ed', 'h2')
+print(r)
+
+r = set_series_kv('77.000', 'send_time', 'ed', 'h3')
+print(r)
+
+r = get_series_kv('send_time')
+print(r)
+r = get_series_kv('send_time', 'ed')
+print(r)
+r = get_series_kv('send_time', 'ed', 'h2')
+print(r)
+r = get_series_kv('send_time', 'ed', 'h3')
 print(r)
